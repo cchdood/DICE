d3e00625571339ef4cda2ff479bf2b119bf4c161
cchdood <96824745+cchdood@users.noreply.github.com>
2022-12-06 14:24:03 +0800

    Update RBC.cpp

--- RBC/RBC.cpp
+++ RBC/RBC.cpp
@@ -1,4 +1,4 @@
-﻿#include <glad/glad.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <stb/stb_image.h>
@@ -16,30 +16,23 @@
 #include "Shader.h"
 
 #include "Keyboard.h"
-#include "Mouse.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-void setTexture(unsigned int texture, const char* path);
+void setTexture(unsigned int &texture, const char* path);
 
 glm::mat4 mouseTransform = glm::mat4(1.0f);
 
 unsigned int screen_width = 800, screen_height = 600;
-float x, y, z, rad;
+float x, y, z, dx, dy, dz, rad, cam_rad;
 
 int main(void)
 {
 
     GLFWwindow* window;
 
-    /* Initialize the library */
+    // Initialize the library 
     glfwInit();
-
-    // opengl version 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(screen_width, screen_height, "Hello World", NULL, NULL);
@@ -64,77 +57,86 @@ int main(void)
 
     glfwSetKeyCallback(window, Keyboard::keyCallback);
 
-    glfwSetCursorPosCallback(window, Mouse::cursorPosCallback);
-    glfwSetMouseButtonCallback(window, Mouse::mouseButtonCallback);
-    glfwSetScrollCallback(window, Mouse::mouseWheelCallback);
-
     glEnable(GL_DEPTH_TEST);
-    /* 
-    shader 
-    */
 
+    // complie vertex shader and fragment shader
     Shader shader("assets/vertex_core.glsl", "assets/fragment_core.glsl");
 
-    //vertices
+    //data matrix
     float v1[] = {
         // position                texture         face index
 
-        // face 1
+        // cube face 1
         -0.5f, -0.5f, -0.5f,       0.0f, 0.0f,     1.0f,
          0.5f, -0.5f, -0.5f,       1.0f, 0.0f,     1.0f,
          0.5f,  0.5f, -0.5f,       1.0f, 1.0f,     1.0f,
         -0.5f,  0.5f, -0.5f,       0.0f, 1.0f,     1.0f,
 
-        // face 2
+        // cube face 2
         -0.5f, -0.5f,  0.5f,       0.0f, 0.0f,     2.0f,
          0.5f, -0.5f,  0.5f,       1.0f, 0.0f,     2.0f,
          0.5f,  0.5f,  0.5f,       1.0f, 1.0f,     2.0f,
         -0.5f,  0.5f,  0.5f,       0.0f, 1.0f,     2.0f,
 
-        // face 3
+        // cube face 3
         -0.5f, -0.5f, -0.5f,       0.0f, 0.0f,     3.0f,
          0.5f, -0.5f, -0.5f,       1.0f, 0.0f,     3.0f,
          0.5f, -0.5f,  0.5f,       1.0f, 1.0f,     3.0f,
         -0.5f, -0.5f,  0.5f,       0.0f, 1.0f,     3.0f,
 
-        // face 4
+        // cube face 4
         -0.5f,  0.5f, -0.5f,       0.0f, 0.0f,     4.0f,
          0.5f,  0.5f, -0.5f,       1.0f, 0.0f,     4.0f,
          0.5f,  0.5f,  0.5f,       1.0f, 1.0f,     4.0f,
         -0.5f,  0.5f,  0.5f,       0.0f, 1.0f,     4.0f,
 
-        // face 5
+        // cube face 5
         -0.5f, -0.5f, -0.5f,       0.0f, 0.0f,     5.0f,
         -0.5f,  0.5f, -0.5f,       1.0f, 0.0f,     5.0f,
         -0.5f,  0.5f,  0.5f,       1.0f, 1.0f,     5.0f,
         -0.5f, -0.5f,  0.5f,       0.0f, 1.0f,     5.0f,
 
-        // face 6
+        // cube face 6
          0.5f, -0.5f, -0.5f,       0.0f, 0.0f,     6.0f,
          0.5f,  0.5f, -0.5f,       1.0f, 0.0f,     6.0f,
          0.5f,  0.5f,  0.5f,       1.0f, 1.0f,     6.0f,
-         0.5f, -0.5f,  0.5f,       0.0f, 1.0f,     6.0f
+         0.5f, -0.5f,  0.5f,       0.0f, 1.0f,     6.0f,
+
+        // tetra
+         1.0f,  0.0f,  0.0f,       0.3f, 0.5f,     0.0f,
+         0.0f,  1.0f,  0.0f,       0.2f, 0.7f,     0.0f,
+         0.0f,  0.0f,  1.0f,       0.1f, 0.3f,     0.0f,
+         0.0f,  0.0f,  0.0f,       0.4f, 0.8f,     0.0f
         
     }; 
 
     unsigned int indices[] = {
-    0,  1,  3,   // face 1
+    0,  1,  3,   // cube face 1
     1,  2,  3,   
 
-    4,  5,  7,   // face 2
+    4,  5,  7,   // cube face 2
     5,  6,  7,
 
-    8,  9,  11,   // face 3
+    8,  9,  11,  // cube face 3
     9,  10, 11,
 
-    12, 13, 15,   // face 4
+    12, 13, 15,  // cube face 4
     13, 14, 15,
 
-    16, 17, 19,  // face 5
+    16, 17, 19,  // cube face 5
     17, 18, 19,
 
-    20, 21, 23,  // face 6
-    21, 22, 23
+    20, 21, 23,  // cube face 6
+    21, 22, 23,
+
+    24, 25, 26,  // tetra face 1
+
+    24, 25, 27,  // tetra face 2
+
+    24, 26, 27,  // tetra face 3
+
+    25, 26, 27   // tetra face 4
+
     };
 
     //create Vertex Buffer Obj and Vertex Array Obj
@@ -166,105 +168,15 @@ int main(void)
     glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(5 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
-    //texture
+    // texture
     unsigned int texture1, texture2, texture3, texture4, texture5, texture6;
 
-    glGenTextures(1, &texture1);                                        // gen texture
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // wrap
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   // filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    int width, height, nChannels;                                       // load image
-    unsigned char* data = stbi_load("image/1.png", &width, &height, &nChannels, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else std::cout << "error occurs while loading the texture" << std::endl;
-    stbi_image_free(data);
-
-
-    glGenTextures(1, &texture2);                                        // gen texture
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // wrap
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   // filtering   
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    width, height, nChannels;                                       // load image
-    data = stbi_load("image/2.png", &width, &height, &nChannels, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else std::cout << "error occurs while loading the texture" << std::endl;
-    stbi_image_free(data);
-
-
-    glGenTextures(1, &texture3);                                        // gen texture
-    glBindTexture(GL_TEXTURE_2D, texture3);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // wrap
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   // filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    width, height, nChannels;                                       // load image
-    data = stbi_load("image/3.png", &width, &height, &nChannels, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else std::cout << "error occurs while loading the texture" << std::endl;
-    stbi_image_free(data);
-
-
-    glGenTextures(1, &texture4);                                        // gen texture
-    glBindTexture(GL_TEXTURE_2D, texture4);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // wrap
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   // filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    width, height, nChannels;                                       // load image
-    data = stbi_load("image/4.png", &width, &height, &nChannels, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else std::cout << "error occurs while loading the texture" << std::endl;
-    stbi_image_free(data);
-
-
-    glGenTextures(1, &texture5);                                        // gen texture
-    glBindTexture(GL_TEXTURE_2D, texture5);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // wrap
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   // filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    width, height, nChannels;                                       // load image
-    data = stbi_load("image/5.png", &width, &height, &nChannels, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else std::cout << "error occurs while loading the texture" << std::endl;
-    stbi_image_free(data);
-
-
-    glGenTextures(1, &texture6);                                        // gen texture
-    glBindTexture(GL_TEXTURE_2D, texture6);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // wrap
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   // filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    width, height, nChannels;                                       // load image
-    data = stbi_load("image/6.png", &width, &height, &nChannels, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else std::cout << "error occurs while loading the texture" << std::endl;
-    stbi_image_free(data);
-
-
+    setTexture(texture1, "image/1.png");
+    setTexture(texture2, "image/2.png");
+    setTexture(texture3, "image/3.png");
+    setTexture(texture4, "image/4.png");
+    setTexture(texture5, "image/5.png");
+    setTexture(texture6, "image/6.png");
 
     shader.activate();
     shader.setInt("texture1", 0);
@@ -274,10 +186,16 @@ int main(void)
     shader.setInt("texture5", 4);
     shader.setInt("texture6", 5);
 
+    // initialize parameters for transition matrix
+
     x = 0.0f;
     y = -0.3f;
     z = 7.0f;
-    rad = -55.0f;
+    dx = 1.0f;
+    dy = 1.0f;
+    dz = 1.0f;
+    rad = 60.0f;
+    cam_rad = 15.0f;
 
     while (!glfwWindowShouldClose(window))
     {
@@ -285,7 +203,7 @@ int main(void)
         processInput(window);
 
         //rendering
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);    // empty buffer color
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);    // empty buffer color
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
         glActiveTexture(GL_TEXTURE0);
@@ -304,25 +222,35 @@ int main(void)
         //draw
         glBindVertexArray(VAO);
 
-        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 model = glm::mat4(1.0f);       // initialized as identity matrix 
+        glm::mat4 model_tetra = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
 
-        model = glm::rotate(model, glm::radians(rad), glm::vec3(0.5f));
-        view = glm::translate(view, glm::vec3(-x, -y, -z));
-        projection = glm::perspective(glm::radians(45.0f), (float)screen_width / (float)screen_height, 0.1f, 100.0f);
+        model_tetra = glm::translate(model_tetra, glm::vec3(-1.0f, -1.0f, -1.0f));                   // model matrix for the tetra
+        model_tetra = glm::rotate(model_tetra, glm::radians(60.0f), glm::vec3(1.0f, 2.0f, 1.0f));
+
+        model = glm::translate(model, glm::vec3(dx, dy, dz));                                        // model matrix for the dice
+        model = glm::rotate(model, glm::radians(rad), glm::vec3(0.0f, 1.0f, 1.0f));
+
+        view = glm::translate(view, glm::vec3(-x, -y, -z));                                          // view matrix
+        view = glm::rotate(view, glm::radians(cam_rad), glm::vec3(1.0f, 0.0f, 0.0f));
+        
+        projection = glm::perspective(glm::radians(45.0f), (float)screen_width / (float)screen_height, 0.1f, 100.0f);     // projection matrix
+        // projection = glm::ortho(glm::radians(45.0f), (float)screen_width / (float)screen_height, 0.1f, 100.0f);
 
         shader.activate();
 
         shader.setMat4("model", model);
+        shader.setMat4("model_tetra", model_tetra);
         shader.setMat4("view", view);
         shader.setMat4("projection", projection);
 
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 48, GL_UNSIGNED_INT, 0);
 
         glBindVertexArray(0);
 
-        //new frame to window
+        // new frame to window
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -346,37 +274,41 @@ void processInput(GLFWwindow* window)
     }
 
     if (Keyboard::key(GLFW_KEY_UP)) {
-        z += 0.1f;
+        dz -= 0.1f;
     }
 
     if (Keyboard::key(GLFW_KEY_DOWN)) {
-        z -= 0.1f;
+        dz += 0.1f;
     }
 
     if (Keyboard::key(GLFW_KEY_LEFT)) {
-        x += 0.1f;
+        dx -= 0.1f;
     }
 
     if (Keyboard::key(GLFW_KEY_RIGHT)) {
-        x -= 0.1f;
+        dx += 0.1f;
     }
 
     if (Keyboard::key(GLFW_KEY_SPACE)) {
-        y -= 0.1f;
+        dy += 0.1f;
     }
 
     if (Keyboard::key(GLFW_KEY_LEFT_CONTROL)) {
-        y += 0.1f;
+        dy -= 0.1f;
     }
 
     if (Keyboard::key(GLFW_KEY_E)) {
+        rad -= 1.0f;
+    }
+
+    if (Keyboard::key(GLFW_KEY_Q)) {
         rad += 1.0f;
     }
 }
 
 void setTexture(unsigned int &texture, const char* path) {
 
-    glGenTextures(1, &texture);                                        // gen texture
+    glGenTextures(1, &texture);                                         // gen texture
     glBindTexture(GL_TEXTURE_2D, texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);       // wrap
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -391,6 +323,3 @@ void setTexture(unsigned int &texture, const char* path) {
     else std::cout << "error occurs while loading the texture" << std::endl;
     stbi_image_free(data);
 }
-
-
-
